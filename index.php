254f612e4431fe0838ae37b0f161c352e0d47603
Steven <steven.dias@ynov.com>
2019-01-30 12:14:47 +0100

    add project files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_GET['page']) AND !empty($_GET['page'])) {
+    $page = strtolower($_GET['page']); // HOME
+} else {
+    $page ='home';
+}
+
+$allPages = scandir('controllers/');
+
+if (in_array($page.'_controller.php',$allPages)) {
+    //inclusion de la page
+    include_once 'controllers/'.$page.'_controller.php';
+    include_once 'Models/'.$page.'_model.php';
+    include_once 'Views/'.$page.'_view.php';
+} else {
+    //retour d'une erreur
+    echo'Erreur 404';
+}
