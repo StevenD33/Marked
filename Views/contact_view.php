684d516ccd065571be834532e40fb362afede2e3
Steven <steven.dias@ynov.com>
2019-01-30 14:11:05 +0100

    Ajout d'un menu et d'une page contact

---
+++ Views/contact_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= ucfirst($page) ?> - Mon site !</title>
+</head>
+<body>
+<header>
+
+</header>
+
+<h1>Contact</h1>
+<footer>
+
+</footer>
+</body>
+</html>
--- Views/home_view.php
+++ Views/home_view.php
@@ -4,6 +4,16 @@
     <title><?= ucfirst($page) ?> - Mon site !</title>
 </head>
 <body>
+<header>
+    <ul>
+        <li><a href="index.php?page=home">Accueil</a> </li>
+        <li><a href="index.php?page=contact">Contact</a> </li>
+    </ul>
+</header>
+
     <h1>Hello world !</h1>
+<footer>
+
+</footer>
 </body>
 </html>
